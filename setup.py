365062091d66a999e2129b31938b4ba3d90c9302
Hugo Ledoux <h.ledoux@tudelft.nl>
2019-06-13 09:19:46 +0200

    Bump version + align with startin version
    simpler and clearer

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ tests_require = install_requires + ["pytest", "pytest-benchmark"]
 
 setup(
     name="startin",
-    version="0.1.4",
+    version="0.4.2",
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Development Status :: 3 - Alpha",
