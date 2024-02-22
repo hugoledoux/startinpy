4c1c09270cc934aaca9805114c42bb78e06ae224
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-02-22 16:54:34 +0100

    Improve grid.py demo

--- demo/grid.py
+++ demo/grid.py
@@ -11,24 +11,24 @@ def main():
 
     #-- read intensity and store it as extra_attribute in the startinpy DT
     d = np.vstack((las.x, las.y, las.z)).transpose()
-    d = d[::1] #-- thinning to speed up, put ::1 to keep all the points
+    d = d[::10] #-- thinning to speed up, put ::1 to keep all the points
 
     dt = startinpy.DT()
     dt.duplicates_handling = "Highest"
-    # dt.insert(d)
+    print("Constructing the TIN with {} points".format(len(d)))
     for each in tqdm(d):
         dt.insert_one_pt(each[0], each[1], each[2])
 
     #-- grid with 1m resolution the bbox
     bbox = dt.get_bbox()
-    cellsize = 0.5
+    cellsize = 1.0
     deltax = math.ceil((bbox[2] - bbox[0]) / cellsize)
     deltay = math.ceil((bbox[3] - bbox[1]) / cellsize)
     # zhat = numpy.zeros(shape=(deltay,deltax))
 
     centres = []
     i = 0
-    for row in tqdm(range((deltay - 1), -1, -1)):
+    for row in range((deltay - 1), -1, -1):
         j = 0
         y = bbox[1] + (row * cellsize) + (cellsize / 2)
         for col in range(deltax):
@@ -37,11 +37,17 @@ def main():
             j += 1
         i += 1
     centres = np.asarray(centres)
+    print("Interpolating at {} locations".format(centres.shape[0]))
     zhat = dt.interpolate({"method": "TIN"}, centres)
-    write_asc_file('grid.tiff', zhat.reshape((deltay, deltax)), (bbox[0], bbox[1]), cellsize)
+    
+    #-- save to a GeoTIFF with rasterio
+    write_rasterio('grid2.tiff', zhat.reshape((deltay, deltax)), (bbox[0], bbox[1]), cellsize)
+    #-- save to a ASC grid file (text file)
+    # write_asc_file('grid.asc', zhat.reshape((deltay, deltax)), (bbox[0], bbox[1]), cellsize)
 
-def write_asc_file(namefile, a, lowerleft, cellsize):
-    fout = open(namefile, 'w')
+
+def write_asc_file(output_file, a, lowerleft, cellsize):
+    fout = open(output_file, 'w')
     fout.write('NCOLS %d\n' % a.shape[1])
     fout.write('NROWS %d\n' % a.shape[0])
     fout.write('XLLCORNER %f\n' % lowerleft[0])
@@ -57,6 +63,20 @@ def write_asc_file(namefile, a, lowerleft, cellsize):
                 fout.write(str(each) + ' ')
         fout.write('\n')
     fout.close()
+
+
+def write_rasterio(output_file, a, bbox, cellsize):
+    with rasterio.open(output_file, 'w', 
+                       driver='GTiff', 
+                       height=a.shape[0],
+                       width=a.shape[1], 
+                       count=1, 
+                       dtype=np.float32,
+                       crs=rasterio.crs.CRS.from_string("EPSG:28992"), 
+                       nodata=np.nan,
+                       transform=(cellsize, 0., bbox[0], 0., -cellsize, bbox[1])) as dst:
+        dst.write(a, 1)
+    print("File written to '%s'" % output_file)
 
 
 if __name__ == '__main__':
