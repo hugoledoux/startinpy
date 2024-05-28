60ecdc18ea28a1bd8cc712f490b72b3b4f09275c
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-05-28 17:23:21 +0200

    WIP demo code for the attributes

--- demo/attributes.py
+++ demo/attributes.py
@@ -6,27 +6,28 @@ import laspy
 las = laspy.read("../data/small.laz")
 
 #-- read intensity and store it as extra_attribute in the startinpy DT
-d = np.vstack((las.x, las.y, las.z, las.intensity)).transpose()
+d = np.vstack((las.x, las.y, las.z, las.classification)).transpose()
 d = d[::1] #-- thinning to speed up, put ::1 to keep all the points
 
+print(d)
+
 dt = startinpy.DT()
-dt.add_attribute_map([("intensity", "f64")])
+dt.add_attribute_map([("classification", "u64")])
+# dt.add_attribute_map([("intensity", "f64")])
 
 for each in d:
-    dt.insert_one_pt(each[:3], intensity=each[3])
+    dt.insert_one_pt(each[:3], classification=int(each[3]))
 
 print("done")
 # a = {'intensity': 155.5, 'reflectance': 111, 'something': True}
-dt.add_vertex_attributes(50, intensity=112.2)
+dt.add_vertex_attributes(50, classification=int(112.2))
 
 print(dt)
-
-# print("all extra attributes:", dt.list_attributes())
+print(dt.get_attribute_map())
+print(dt.attributes[1:])
 
 a = dt.get_vertex_attributes(50)
 print("=>", a)
 a = dt.get_vertex_attributes(49)
 print("=>", a)
 
-i = dt.attribute('intensity')
-print(i.shape)
--- demo/temp.py
+++ demo/temp.py
@@ -7,16 +7,17 @@ import json
 
 
 dt = startinpy.DT()
-dt.add_attribute_map([("intensity", "f64"), ("visited", "i64")])
+dt.add_attribute_map([("intensity", "f64")])
+# dt.add_attribute_map([("intensity", "f64"), ("visited", "bool")])
 
-dt.insert_one_pt([20.0, 30.0, 1.1], intensity=111.2, visited=4);
-dt.insert_one_pt([120.0, 33.0, 12.5], intensity=111.2, visited=5);
-dt.insert_one_pt([124.0, 222.0, 7.65], intensity=111.2, visited=6);
-dt.insert_one_pt([20.0, 133.0, 21.0], intensity=111.2, visited=7);
-dt.insert_one_pt([23.0, 13.0, 11.0], intensity=111.2, visited=8);
-dt.insert_one_pt([60.0, 60.0, 33.0], intensity=111.2, visited=9);
+dt.insert_one_pt([20.0, 30.0, 1.1], intensity=11.1, visited=True);
+dt.insert_one_pt([120.0, 33.0, 12.5], intensity=22.2, visited=True);
+dt.insert_one_pt([124.0, 222.0, 7.65], intensity=33.3, visited=True);
+dt.insert_one_pt([20.0, 133.0, 21.0], intensity=44.4, visited=True);
+dt.insert_one_pt([23.0, 13.0, 11.0], intensity=55.5, visited=False);
+dt.insert_one_pt([60.0, 60.0, 33.0], intensity=66.6, visited=False);
 
 
 print(dt)
-
-print(dt.all_attributes()[1:])
+print(dt.get_attribute_map())
+print(dt.attributes)
