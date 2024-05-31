92b95d79ba77fdaeda6e701d89618a05a34948ea
Hugo Ledoux <h.ledoux@tudelft.nl>
2024-05-31 11:42:58 +0200

    Update demo file

--- demo/temp.py
+++ demo/temp.py
@@ -13,15 +13,16 @@ print(d)
 dt.set_attributes_schema(d)
 
 dt.insert_one_pt([20.0, 30.0, 1.1], classification=11.1, visited=True, intensity='12345678901');
-dt.insert_one_pt([120.0, 33.0, 12.5], classification=22.2, visited=True, intensity='a');
-dt.insert_one_pt([124.0, 222.0, 7.65], classification=33.3, visited=True, intensity='a');
-dt.insert_one_pt([20.0, 133.0, 21.0], classification=44.4, visited=True, intensity='a');
-dt.insert_one_pt([23.0, 13.0, 11.0], classification=55.5, visited=False, intensity='a');
-dt.insert_one_pt([60.0, 60.0, 33.0], classification=66.6, visited=False, intensity='a');
+vi, b = dt.insert_one_pt([120.0, 33.0, 12.5]);
+dt.set_vertex_attributes(vi, classification=22.2, visited=True, intensity='a');
+# dt.insert_one_pt([120.0, 33.0, 12.5], classification=22.2, visited=True, intensity='a');
+dt.insert_one_pt([124.0, 222.0, 7.65], classification=33.3, visited=True, intensity='b');
+dt.insert_one_pt([20.0, 133.0, 21.0], classification=44.4, visited=True, intensity='c');
+dt.insert_one_pt([23.0, 13.0, 11.0], classification=55.5, visited=False, intensity='d');
+dt.insert_one_pt([60.0, 60.0, 33.0], classification=66.6, visited=False, intensity='e');
 
 
 print(dt)
-# print("=>", dt.get_attributes_schema())
 print(dt.attributes)
+print("=>", dt.get_attributes_schema())
 print(dt.attributes.dtype)
-#
